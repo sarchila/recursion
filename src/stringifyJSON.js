42183c7efe784ca36e993884b89f3e60d1979d55
Santiago <santiago.archila@gmail.com>
2013-11-01 21:12:16 -0700

    inital attempt handles primitives, strings, and nested arrays

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,24 @@
 
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
-  // your code goes here
+  var finalString ;
+  if (obj === undefined) {
+  	finalString = "";
+  } else if(Array.isArray(obj)){
+  	console.log("first branch");
+  	var reducedStr = _.reduce(obj, function(str,elem,ind){
+  		if (ind==0){
+  			return str + stringifyJSON(elem);
+  		} else {
+  			return str + "," + stringifyJSON(elem);
+  		}
+  	}, "");
+  	finalString = "[" + reducedStr + "]";
+  } else if (typeof(obj)=="string") {
+  	finalString = "\"" + obj + "\""
+  } else {
+  	console.log("second branch");
+  	finalString = obj + "";
+  }
+  return finalString;
 };
