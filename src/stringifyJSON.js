42667cf4b339961c0ec5eee59b2ba0aafc067da0
Santiago <santiago.archila@gmail.com>
2013-11-03 23:07:31 -0800

    handle Object type (passes all validObjects from fixtures.js)
    Still failing the weirdObjects test case

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -15,6 +15,17 @@ var stringifyJSON = function (obj) {
   		}
   	}, "");
   	finalString = "[" + reducedStr + "]";
+  } else if (typeof(obj)==="object" && !Array.isArray(obj) && obj != null){
+  	var firstTime = true;
+  	var reducedStr = _.reduce(obj, function(str,elem,ind){
+  		if (firstTime){
+  			firstTime = false;
+  			return str + stringifyJSON(ind) + ":" + stringifyJSON(elem);
+  		} else {
+  			return str + "," + stringifyJSON(ind) + ":" + stringifyJSON(elem);
+  		}  		
+  	}, "");
+  	finalString = "{" + reducedStr + "}";
   } else if (typeof(obj)=="string") {
   	finalString = "\"" + obj + "\""
   } else {
